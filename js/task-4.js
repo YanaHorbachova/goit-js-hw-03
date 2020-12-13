a2a2d632bfb1fb6f1a5a52476713c3f6466a6440
YanaHorbachova <maya69877@gmail.com>
2020-12-13 22:17:35 +0200

    сделано задание 4

---
+++ js/task-4.js
@@ -0,0 +1,29 @@
+const countTotalSalary = function(employees) {
+    const values = Object.values(employees);
+    let totalSalary = 0; 
+
+    for (const velue of values) {
+        totalSalary += velue;
+    };
+
+    return totalSalary
+   
+  };
+  
+  console.log(countTotalSalary({})); // 0
+  
+  console.log(
+    countTotalSalary({
+      mango: 100,
+      poly: 150,
+      alfred: 80,
+    }),
+  ); // 330
+  
+  console.log(
+    countTotalSalary({
+      kiwi: 200,
+      lux: 50,
+      chelsy: 150,
+    }),
+  ); // 400
