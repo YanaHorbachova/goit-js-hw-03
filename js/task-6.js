e43e7c1d918d620ba4fdaded4929dc23999fbc8f
YanaHorbachova <maya69877@gmail.com>
2020-12-16 22:34:42 +0200

    сделано задание 6

---
+++ js/task-6.js
@@ -0,0 +1,21 @@
+const products = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Сканер', price: 2700, quantity: 3 },
+    { name: 'Дроид', price: 400, quantity: 7 },
+    { name: 'Захват', price: 1200, quantity: 2 },
+  ];
+  
+  const calculateTotalPrice = function(allProdcuts, productName) {
+    let total = 0;
+    for (const products of allProdcuts) {
+        if (productName === products.name) {
+            total += products.price * products.quantity;
+        }
+    }
+    return total;
+  };
+  
+
+  console.log(calculateTotalPrice(products, 'Радар')); // 5200
+  
+  console.log(calculateTotalPrice(products, 'Дроид')); // 2800
